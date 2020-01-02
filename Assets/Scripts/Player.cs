c09ef6d0ae52e4892cd12a6cdeb1e22675dc41da
Roman Saykin <sc2aru@gmail.com>
2020-01-03 02:43:52 +0300

    player now jump once

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,13 +10,14 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D _rb2d;
     private Animator _animator;
-
+    private Collider2D _collider;
     
 
     private void Awake()
     {
         _rb2d = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        _collider = GetComponent<Collider2D>();
     }
 
     private void FixedUpdate()
@@ -30,26 +31,31 @@ public class Player : MonoBehaviour
         float direction = CrossPlatformInputManager.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(direction * _speed, _rb2d.velocity.y);
         _rb2d.velocity = playerVelocity;
+
         SwapFacing(direction);
-        bool isRunning = Mathf.Abs(_rb2d.velocity.x) > Mathf.Epsilon;
-        _animator.SetBool("Running", isRunning);
+
+        _animator.SetBool("Running", IsRunning());
     }
 
     private void SwapFacing(float direction)
     {
-        bool playerHorizontalSpeed = Mathf.Abs(_rb2d.velocity.x) > Mathf.Epsilon;
-        if (playerHorizontalSpeed)
+        if (IsRunning())
             transform.localScale = new Vector2(Mathf.Sign(direction), transform.localScale.y);
+    }
+
+    private bool IsRunning()
+    {
+        return Mathf.Abs(_rb2d.velocity.x) > Mathf.Epsilon;
     }
 
     private void Jump()
     {
+        if (!_collider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+
         if (CrossPlatformInputManager.GetButtonDown("Jump"))
         {
             Vector2 jumpVelocity = new Vector2(0f, _jumpSpeed);
             _rb2d.velocity += jumpVelocity;
         }
     }
-
-
 }
