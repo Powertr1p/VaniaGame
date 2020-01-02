a89c17b440b167b5512beb2fab748c12c25d6682
Roman Saykin <sc2aru@gmail.com>
2020-01-03 02:24:45 +0300

    player now can jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,18 +6,23 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _jumpSpeed = 11f;
+
     private Rigidbody2D _rb2d;
     private Animator _animator;
+
     
+
     private void Awake()
     {
         _rb2d = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         Movement();
+        Jump();
     }
 
     private void Movement()
@@ -26,7 +31,6 @@ public class Player : MonoBehaviour
         Vector2 playerVelocity = new Vector2(direction * _speed, _rb2d.velocity.y);
         _rb2d.velocity = playerVelocity;
         SwapFacing(direction);
-
         bool isRunning = Mathf.Abs(_rb2d.velocity.x) > Mathf.Epsilon;
         _animator.SetBool("Running", isRunning);
     }
@@ -37,4 +41,15 @@ public class Player : MonoBehaviour
         if (playerHorizontalSpeed)
             transform.localScale = new Vector2(Mathf.Sign(direction), transform.localScale.y);
     }
+
+    private void Jump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocity = new Vector2(0f, _jumpSpeed);
+            _rb2d.velocity += jumpVelocity;
+        }
+    }
+
+
 }
