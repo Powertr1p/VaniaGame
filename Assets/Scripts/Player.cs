5e0ab5b716129ff53a00f2ef2cf993b589fe752f
Roman Saykin <sc2aru@gmail.com>
2020-01-02 14:39:20 +0300

    added plugin and created player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5f;
+    private Rigidbody2D _rb2d;
+    
+    private void Awake()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float direction = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(direction * _speed, _rb2d.velocity.y);
+        _rb2d.velocity = playerVelocity;
+    }
+
+}
