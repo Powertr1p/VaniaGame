b3a134de24fef66a6eb913f35b98de261a7229c5
Roman Saykin <sc2aru@gmail.com>
2020-01-06 12:05:17 +0300

    created pickable coin

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    [SerializeField] private AudioClip _coinPickUpSFX;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Player>())
+        {
+            AudioSource.PlayClipAtPoint(_coinPickUpSFX, Camera.main.transform.position);
+            Destroy(gameObject);
+        }
+    }
+}
