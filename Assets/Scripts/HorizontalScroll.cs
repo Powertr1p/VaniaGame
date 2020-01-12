d1fc94f897f6696eb29add39ce7d7beb752f2288
Spoty <spotichka1@gmail.com>
2020-01-12 20:32:33 +0200

    created new level 4
    n1

---
+++ Assets/Scripts/HorizontalScroll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalScroll : MonoBehaviour
+{
+    [Tooltip("Game units per second")]
+    [SerializeField] private float _scrollRate = 1F;
+
+    private bool _isStarted = false;
+
+private void Update()
+{
+    while(_isStarted)
+    {
+        transform.Translate(new Vector2(_scrollRate * Time.deltaTime, 0f));
+    }
+}
+
+private void OnTriggerEnter2D (Collider2D collision)
+{
+    _isStarted = true;
+}
+    
+    private void DestroyItself() => Destroy(gameObject, 20f);
+
+}
