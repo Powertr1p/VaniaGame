07e42e15f9272a90682de80f8b77dcbd25ebfada
Roman Saykin <sc2aru@gmail.com>
2020-02-04 23:55:09 +0300

    Detector now has RequiredOf

--- Assets/Scripts/Detector.cs
+++ Assets/Scripts/Detector.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(HorizontalScroll))]
 public class Detector : MonoBehaviour
 {
     private HorizontalScroll _movingWall;
