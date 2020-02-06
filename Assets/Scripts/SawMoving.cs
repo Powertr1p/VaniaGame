5985be7407a606a98fc76166b3fcb06b620ba6cf
Roman Saykin <sc2aru@gmail.com>
2020-02-06 23:48:25 +0300

    added new assets and created logic for moving saw trap. Prefabed saw trap

---
+++ Assets/Scripts/SawMoving.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class SawMoving : MonoBehaviour
+{
+    [Header("Start and end position of saw")]
+    [Tooltip("Move your saw to final left postion and copy X here")]
+    [SerializeField] private Vector3 _leftBorder;
+    [Tooltip("Move your saw to final right postion and copy X here")]
+    [SerializeField] private Vector3 _rightBorder;
+
+    private Vector3 _currentValue;
+    private int _direction = 1;
+    private float _speed = 0.5f;
+
+    private void Start()
+    {
+        _currentValue = transform.localPosition;
+    }
+
+    private void Update()
+    {
+        _currentValue.x += _speed * Time.deltaTime * _direction;
+        if (_currentValue.x >= _rightBorder.x || _currentValue.x <= _leftBorder.x)
+            _direction = -_direction;
+
+        transform.localPosition = _currentValue;
+    }
+}
+
+
---
+++ Assets/Scripts/SawRotation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawRotation : MonoBehaviour
+{    
+    private void Update()
+    {
+        transform.Rotate(0, 0, 150 * Time.deltaTime);
+    }
+}
