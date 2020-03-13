bedcf8b39bed1dfc57bdd9d77e9d617b6f9b38f9
Roman Saykin <sc2aru@gmail.com>
2020-03-13 10:33:56 +0300

    refactored saw classes

--- Assets/Scripts/SawMoving.cs
+++ Assets/Scripts/SawMoving.cs
@@ -7,23 +7,33 @@ public class SawMoving : MonoBehaviour
     [SerializeField] private Vector3 _leftBorder;
     [Tooltip("Move your saw to final right postion and copy X here")]
     [SerializeField] private Vector3 _rightBorder;
+    [SerializeField] private float _speed = 0.4f;
 
     private Vector3 _currentValue;
     private int _direction = 1;
-    private float _speed = 0.5f;
 
     private void Start()
     {
         _currentValue = transform.localPosition;
     }
 
-    private void Update()
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
     {
         _currentValue.x += _speed * Time.deltaTime * _direction;
-        if (_currentValue.x >= _rightBorder.x || _currentValue.x <= _leftBorder.x)
-            _direction = -_direction;
+        TryChangeDirection();
 
         transform.localPosition = _currentValue;
+    }
+
+    private void TryChangeDirection()
+    {
+        if (_currentValue.x >= _rightBorder.x || _currentValue.x <= _leftBorder.x)
+            _direction = -_direction;
     }
 }
 
--- Assets/Scripts/SawRotation.cs
+++ Assets/Scripts/SawRotation.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SawRotation : MonoBehaviour
-{    
-    private void Update()
+{
+    private float _speed = 150.0f;
+
+    private void FixedUpdate()
     {
-        transform.Rotate(0, 0, 150 * Time.deltaTime);
+        transform.Rotate(0, 0, _speed * Time.deltaTime, Space.Self);
     }
 }
