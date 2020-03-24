13e5a2ce99f6ced8acf424699d8922f0f172b5e4
Roman Saykin <sc2aru@gmail.com>
2020-03-24 23:24:03 +0300

    refactored dash mechanics

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -59,7 +59,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (_isDashing) return;
 
-        _rb2d.velocity = GetPlayerVelocityBasedOnDirection(direction);
+        _rb2d.velocity = GetPlayerVelocityBasedOnDirection(direction, _movementSpeed);
 
         SwapSpriteFacing(direction);
 
@@ -71,9 +71,9 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector2(Mathf.Sign(direction), transform.localScale.y);
     }
 
-    private Vector2 GetPlayerVelocityBasedOnDirection(float direction)
+    private Vector2 GetPlayerVelocityBasedOnDirection(float direction, float movementSpeed)
     {
-        return new Vector2(direction * _movementSpeed, _rb2d.velocity.y);
+        return new Vector2(direction * movementSpeed, _rb2d.velocity.y);
     }
 
     public IEnumerator TryDash(float direction)
@@ -83,19 +83,18 @@ public class PlayerMovement : MonoBehaviour
         _canDash = false;
         _isDashing = true;
 
-        _movementSpeed += _dashSpeed;
         _rb2d.gravityScale = 0;
         _rb2d.velocity = Vector2.zero;
-
-        PerformDash(transform.localScale.x);
+        //разобраться с локалскейл и взять последнее сохр не нулевое значение инпута
+        _rb2d.velocity = GetPlayerVelocityBasedOnDirection(transform.localScale.x, _movementSpeed + _dashSpeed);
         yield return new WaitForSeconds(_dashingTime);
 
         StartCoroutine(TryStopDash());
     }
 
-    private void PerformDash(float direction)
+    private void PerformDash(float direction, float movementSpeed)
     {
-        _rb2d.velocity = GetPlayerVelocityBasedOnDirection(direction);
+        
     }
 
     private IEnumerator TryStopDash()
