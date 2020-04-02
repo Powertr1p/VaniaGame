32cdca429ddc6e691dc8a7d426a35c8814bd0c8c
Roman Saykin <sc2aru@gmail.com>
2020-04-02 22:09:35 +0300

    fixed timer collision and added variable to store original value

--- Assets/Scripts/Collisions.cs
+++ Assets/Scripts/Collisions.cs
@@ -9,14 +9,15 @@ public class Collisions : MonoBehaviour
     [SerializeField] private Vector2 _bottomOffset;
     [SerializeField] private Vector2 _rightOffset;
     [SerializeField] private Vector2 _leftOffset;
-    [SerializeField] private float _collisionRadius = 0.25f;
-    float _wallSlideTimer = 2f;
+    [SerializeField] private float _collisionRadius;
+    
+    private float _wallSlideResidualCollisionTimer = 0.3f;
+    private float _wallSlideResidualCollisionTimerValue;
    
     private bool _isGrounded;
     private bool _isOnLeftWall;
     private bool _isOnRightWall;
     private bool _isOnWall;
-    public bool CanResuedeSlide;
 
     private Rigidbody2D _rb2d;
     private float _facingDirection;
@@ -27,6 +28,7 @@ public class Collisions : MonoBehaviour
     private void Start()
     {
         _rb2d = GetComponent<Rigidbody2D>();
+        _wallSlideResidualCollisionTimerValue = _wallSlideResidualCollisionTimer;
     }
 
     private void FixedUpdate()
@@ -38,26 +40,27 @@ public class Collisions : MonoBehaviour
         _facingDirection = transform.localScale.x;
 
         CheckForWallSlide();
-        ResetWallSlideTimer();
+        
+        if (_wallSlideResidualCollisionTimer < 0)
+            TryResetWallSlideCollisionTimer();
     }
 
-    private void CheckForWallSlide() //первичный прототип на коленке, надо отрефакторить нормально (пиздец мне стыдно за эту хуйню)
+    private void CheckForWallSlide() //первичный прототип на коленке, надо отрефакторить нормально
     {
-        if (_isOnRightWall && _facingDirection == 1 && _rb2d.velocity.y < 0 && !_isGrounded )
+        if (_isOnRightWall && _facingDirection == 1 && _rb2d.velocity.y < 0 && !_isGrounded)
         {
             if (_rb2d.velocity.x > 0)
             {
                 _isOnWall = true;
             }
-            else if (_rb2d.velocity.x == 0 && _wallSlideTimer > 0)
+            else if (_rb2d.velocity.x == 0 && _wallSlideResidualCollisionTimer > 0)
             {
-                ReduceWallSliderTimer();
-                _isOnWall = true;
+                ToggleResidualCollisionAndStartTimer();
             }
             else
             {
                 _isOnWall = false;
-            } 
+            }
         }
         else if (_isOnLeftWall && _facingDirection == -1 && _rb2d.velocity.y < 0 && !_isGrounded)
         {
@@ -65,10 +68,9 @@ public class Collisions : MonoBehaviour
             {
                 _isOnWall = true;
             }
-            else if (_rb2d.velocity.x == 0 && _wallSlideTimer > 0)
+            else if (_rb2d.velocity.x == 0 && _wallSlideResidualCollisionTimer > 0)
             {
-                ReduceWallSliderTimer();
-                _isOnWall = true;
+                ToggleResidualCollisionAndStartTimer();
             }
             else
             {
@@ -86,6 +88,26 @@ public class Collisions : MonoBehaviour
         return Physics2D.OverlapCircle(position, collisionRadius, layer);
     }
 
+    private void ToggleResidualCollisionAndStartTimer()
+    {
+        ReduceWallSliderCollisionTimer();
+        _isOnWall = true;
+    }
+
+    private void ReduceWallSliderCollisionTimer()
+    {
+        if (_wallSlideResidualCollisionTimer >= 0)
+            _wallSlideResidualCollisionTimer -= Time.deltaTime;
+
+        Debug.Log(_wallSlideResidualCollisionTimer);
+    }
+
+    private void TryResetWallSlideCollisionTimer() //прототип
+    {
+        if (!_isOnLeftWall && !_isOnRightWall || _isGrounded)
+            _wallSlideResidualCollisionTimer = _wallSlideResidualCollisionTimerValue;
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
@@ -93,21 +115,5 @@ public class Collisions : MonoBehaviour
         Gizmos.DrawWireSphere((Vector2)transform.position + _bottomOffset, _collisionRadius);
         Gizmos.DrawWireSphere((Vector2)transform.position + _rightOffset, _collisionRadius);
         Gizmos.DrawWireSphere((Vector2)transform.position + _leftOffset, _collisionRadius);
-    }
-
-    private void ReduceWallSliderTimer()
-    {
-        if (_wallSlideTimer >= 0)
-        {
-            _wallSlideTimer -= 0.1f;
-        }
-    }
-
-    private void ResetWallSlideTimer() //прототип
-    {
-        if (!_isOnLeftWall && !_isOnRightWall && _wallSlideTimer <= 0)
-        {
-            _wallSlideTimer = 2f;
-        }
     }
 }
