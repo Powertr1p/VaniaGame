2fff834335d1893913113267b765151e57f402ee
Roman Saykin <sc2aru@gmail.com>
2020-04-09 17:15:39 +0300

    created a jumpPad with jumping logic

---
+++ Assets/Scripts/Environment/JumpPad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    [Header("JumpPad Config")]
+    [Tooltip("Сила, с которой джампад выталкивает игрока вверх (от 500 и выше)")]
+    [SerializeField] private float _jumpForce;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        var _rb2d = collision.gameObject.GetComponent<Rigidbody2D>();
+        _rb2d.velocity = Vector2.zero;
+        _rb2d.AddForce(new Vector2(0, _jumpForce));
+    }
+
+}
