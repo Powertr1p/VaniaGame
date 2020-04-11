a0b553ae6e9bb057af03d70602c451cea9a01aa9
Roman Saykin <sc2aru@gmail.com>
2020-04-11 18:48:21 +0300

    refactored abstact JumpPad and inherited a simple jump pad

---
+++ Assets/Scripts/Environment/JumpPad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class JumpPad : MonoBehaviour, IInteractable
+{
+    [SerializeField] protected float JumpForce;
+    protected Rigidbody2D PlayerRigidBody;
+
+    public abstract void Interact();
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        PlayerRigidBody = collision.gameObject.GetComponent<Rigidbody2D>();
+
+        Interact();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PlayerRigidBody = null;
+    }
+}
--- Assets/Scripts/Environment/SimpleJumpPad.cs
+++ Assets/Scripts/Environment/SimpleJumpPad.cs
@@ -1,6 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class SimpleJumpPad : JumpPad
 {
+    public override void Interact()
+    {
+        if (PlayerRigidBody == null) return;
 
+        PlayerRigidBody.velocity = Vector2.zero;
+        PlayerRigidBody.AddForce(new Vector2(0, JumpForce));
+    }
 }
