9faef27000b4ed91a96e4df5bb959218da8d2d53
Roman Saykin <sc2aru@gmail.com>
2020-04-12 14:24:37 +0300

    created a wind trap

---
+++ Assets/Wind.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    [SerializeField] private float _windForce;
+
+    private Rigidbody2D _playerRigidbody;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _playerRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (_playerRigidbody == null) return;
+
+        _playerRigidbody.AddForce(new Vector2(_windForce, 0));
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _playerRigidbody = null;
+    }
+}
