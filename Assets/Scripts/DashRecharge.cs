d993bc94ef7f04d046121448deb92eae54b02c49
Roman Saykin <sc2aru@gmail.com>
2020-04-19 14:50:57 +0300

    refactored a bit DashRecharge class

--- Assets/Scripts/DashRecharge.cs
+++ Assets/Scripts/DashRecharge.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class DashRecharge : MonoBehaviour
 {
+    private const float delayBeforeDestroy = 1f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var player = collision.gameObject.GetComponent<PlayerMovement>();
         if (player != null)
             StartCoroutine(player.RechargeDash());
 
-        Destroy(gameObject, 1f);
+        Destroy(gameObject, delayBeforeDestroy);
         Destroy(gameObject.GetComponent<SpriteRenderer>());
         Destroy(gameObject.GetComponent<CircleCollider2D>());
     }
