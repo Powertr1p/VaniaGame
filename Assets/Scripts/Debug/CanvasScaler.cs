dbbe25b5c69dc390a2436d74c3e0ff6b3ab25079
Roman Saykin <sc2aru@gmail.com>
2020-05-05 13:07:44 +0300

    added debug params on screen

--- Assets/Scripts/Debug/CanvasScaler.cs
+++ Assets/Scripts/Debug/CanvasScaler.cs
@@ -6,25 +6,16 @@ using UnityEngine;
 public class CanvasScaler : MonoBehaviour
 {
     private PlayerMovement _player;
-    private Canvas _canvas;
+    private Transform _transform;
 
     private void Awake()
     {
         _player = GetComponentInParent<PlayerMovement>();
-        _canvas = GetComponent<Canvas>();
+        _transform = GetComponent<Transform>();
     }
 
     void Update()
     {
-        UnityEngine.Debug.Log(_player.transform.localScale.x);
-        
-        if (_player.transform.localScale.x == -1f)
-        {
-            _canvas.transform.localScale = new Vector3(-1f,1f,1f);
-        }
-        else
-        {
-            _canvas.transform.localScale = new Vector3(1f,1f,1f);
-        }
+        _transform.localScale = _player.transform.localScale;
     }
 }
