570af3aeb5f5c89e8dc3b8fa5d2f79496c5e5dc2
Roman Saykin <sc2aru@gmail.com>
2020-05-10 18:23:50 +0300

    makes Constant class static

--- Assets/Scripts/Data/Constants.cs
+++ Assets/Scripts/Data/Constants.cs
@@ -2,14 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Constants
+public static class Constants
 {
     public const string Running = "Running";
-    public const string Climbing = "Climbing";
-    public const string ClimbingSpeed = "ClimbingSpeed";
-    public const string Ladder = "Ladder";
-    public const string Ground = "Ground";
     public const string Died = "Died";
-    public const string Enemy = "Enemy";
     public const string Hazards = "Hazards";
 }
