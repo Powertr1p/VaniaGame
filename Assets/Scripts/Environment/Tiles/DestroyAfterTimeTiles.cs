8c3921732214b8e9d7a8c853984dc690ce6acf65
Powertr1p <sc2aru@gmail.com>
2020-06-07 13:18:48 +0300

    tiles now destroys when player collide with them from any side (including wallslide)

--- Assets/Scripts/Environment/Tiles/DestroyAfterTimeTiles.cs
+++ Assets/Scripts/Environment/Tiles/DestroyAfterTimeTiles.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class DestroyAfterTimeTiles : MonoBehaviour
 {
-    [Tooltip("Time that must passed before destroy tile. Player brokes all block around him (front, back and bottom) at same time")]
+    [Tooltip("Time that must passed before destroy tile. Player broke all block around him (front, back and bottom) at same time")]
     [SerializeField] private float _destroyTime = 0.1f;
     
     private Tilemap _tilemap;
@@ -26,13 +27,34 @@ public class DestroyAfterTimeTiles : MonoBehaviour
     {
         if (_isSuspended) return;
         
-        var tilePosition = GetBottomTile();
+        var tilePosition = FindCollideddTile();
+
         StartCoroutine(WaitAndDestroyTile(tilePosition));
+    }
+
+    private Vector3Int FindCollideddTile()
+    {
+        if (_tilemap.HasTile(GetBottomTile()))
+            return GetBottomTile();
+        else if (_tilemap.HasTile(GetRightTile()))
+            return GetRightTile();
+        else
+            return GetLeftTile();
+    }
+
+    private Vector3Int GetRightTile()
+    {
+        return _tilemap.WorldToCell(_playerPosition + Vector3.right);
+    }
+
+    private Vector3Int GetLeftTile()
+    {
+        return _tilemap.WorldToCell(_playerPosition + Vector3.left);
     }
 
     private Vector3Int GetBottomTile()
     {
-        return _tilemap.WorldToCell(_playerPosition - new Vector3(0f, 1f));
+        return _tilemap.WorldToCell(_playerPosition + Vector3.down);
     }
 
     private IEnumerator WaitAndDestroyTile(Vector3Int tilePosition)
