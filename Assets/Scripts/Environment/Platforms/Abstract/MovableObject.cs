bd5f92c987f7462a63d5a15e743b46b0e9296438
Powertr1p <sc2aru@gmail.com>
2020-06-25 10:00:07 +0300

    created movable platform

---
+++ Assets/Scripts/Environment/Platforms/Abstract/MovableObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MovableObject : MonoBehaviour
+{
+   [SerializeField] protected Transform Waypoint_A;
+   [SerializeField] protected Transform Waypoint_B;
+   [SerializeField] protected float Duration = 5f;
+
+   protected Transform Target;
+   
+   private void Start()
+   {
+      Target = Waypoint_A;
+   }
+
+   protected virtual void Update()
+   {
+      TryChangeTarget();
+      Move();
+   }
+
+   protected void Move()
+   {
+      transform.DOMove(Target.position, Duration);
+   }
+
+   protected void TryChangeTarget()
+   {
+      if (Vector2.Distance(transform.position, Target.position) < 0.5f)
+         ChangeTarget();
+   }
+
+   protected void ChangeTarget()
+   {
+      Target = Target.position == Waypoint_A.position ? Waypoint_B : Waypoint_A;
+   }
+}
