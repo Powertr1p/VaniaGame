b332500949ecfed34f1c9dfecfc8983f6e33d217
Powertr1p <sc2aru@gmail.com>
2020-06-26 11:22:22 +0300

    prefabbed spikes and added delay

---
+++ Assets/Scripts/Environment/Hazards/HiddenSpikes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class HiddenSpikes : MonoBehaviour, ITriggerable
+{
+    private Animator _animator;
+
+    private const string ActivateAnimation = "Activate";
+    private const string DeactivateAnimation = "Deactivate";
+    
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void Activate()
+    {
+        _animator.SetTrigger(ActivateAnimation);
+    }
+
+    public void Deactivate()
+    {
+        _animator.SetTrigger(DeactivateAnimation);
+    }
+}
---
+++ Assets/Scripts/Environment/Hazards/TriggerForObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class TriggerForObject : MonoBehaviour
+{
+    [SerializeField] private GameObject _objectToTrigger;
+    [SerializeField] private float _delayBeforeTrigger = 0f;
+    
+    private ITriggerable _trigger;
+
+    private void Start()
+    {
+        _trigger = _objectToTrigger.GetComponent<ITriggerable>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerMovement>() != null)
+            StartCoroutine(WaitAndToggle(_trigger.Activate));
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerMovement>() != null)
+            StartCoroutine(WaitAndToggle(_trigger.Deactivate));
+    }
+
+    private IEnumerator WaitAndToggle(Action callback)
+    {
+        yield return new WaitForSeconds(_delayBeforeTrigger);
+        callback?.Invoke();
+    }
+}
