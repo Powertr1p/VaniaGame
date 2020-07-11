b5fe899bba5991048a43a0c4a18e78a78673a2e0
Powertr1p <sc2aru@gmail.com>
2020-07-11 13:43:10 +0300

    created level template

---
+++ Assets/Scripts/Data/PrefabSpawner.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Data
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class PrefabSpawner : ScriptableObject
+    {
+        
+    }
+}
