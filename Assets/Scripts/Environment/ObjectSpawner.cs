5c2fff4919627310a79aa709d51f06b2fa8d1bb6
Roman Saykin <sc2aru@gmail.com>
2020-07-16 20:25:31 +0300

    created branch

---
+++ Assets/Scripts/Environment/ObjectSpawner.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _objectToSpawn;
+   
+}
