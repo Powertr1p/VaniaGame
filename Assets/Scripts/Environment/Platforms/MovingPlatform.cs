af3bd33d4a8d2fe8a21d4cdbabf3e120012fc0ca
Powertr1p <sc2aru@gmail.com>
2020-07-24 19:49:04 +0300

    added waypoints logic and added it to basic plats

--- Assets/Scripts/Environment/Platforms/MovingPlatform.cs
+++ Assets/Scripts/Environment/Platforms/MovingPlatform.cs
@@ -1,6 +1,43 @@
+using UnityEngine;
+
 namespace Environment.Platforms
 {
-    public class MovingPlatform : MovableObject
+    public class MovingPlatform : MonoBehaviour
     {
+        [SerializeField] private Waypoint _firstWaypoint;
+
+        private Transform _target => _currentWaypoint.transform;
+        private Waypoint _currentWaypoint;
+        private float _speed;
+
+        private void Start()
+        {
+            _currentWaypoint = _firstWaypoint;
+            _speed = _firstWaypoint.GetSpeed();
+        }
+        
+        private void Update()
+        {
+            Move();
+            TryChangeTarget();
+        }
+        
+        private void Move()
+        {
+            transform.position =  Vector2.MoveTowards(transform.position, _target.transform.position, _speed * Time.deltaTime);
+        }
+
+        private void TryChangeTarget()
+        {
+            if (Vector2.Distance(transform.position, _target.position) < 0.5f)
+                ChangeTarget();
+        }
+
+        private void ChangeTarget()
+        {
+            var nextWaypoint = _currentWaypoint.GetNextWaypoint();
+            _speed = _currentWaypoint.GetSpeed();
+            _currentWaypoint = nextWaypoint != null ? nextWaypoint : _firstWaypoint;
+        }
     }
 }
