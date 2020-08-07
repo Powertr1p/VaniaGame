35ce3b0518dbb8f02add5bbb50995dfe4581f356
Powertr1p <sc2aru@gmail.com>
2020-08-07 11:09:49 +0300

    added parallax script

---
+++ Assets/Scripts/_Visuals/Parallax.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private Transform _camera;
+    [SerializeField] private float _parallaxMovement = 1f;
+
+    private float _cameraOffset;
+    
+    
+    private void Update()
+    {
+        transform.position = new Vector2(_camera.position.x  * _parallaxMovement , transform.position.y);
+    }
+}
